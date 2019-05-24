f92a17af6f7229a76ad5c2bc1032f2c482acce15
JackDing <361164217@qq.com>
2019-05-25 00:32:47 +0800

    商店全选相关功能逻辑

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -11,8 +11,30 @@ import './cart_trade.css'
 new Vue({
   el: ".container",
   data: {
-    cartList:null,
-    editable:false
+    cartList: null,
+    editable: false,
+
+  },
+  computed: {
+    isAll: {
+      //主动设置计算属性时先通过set改变get中用到的data,再通过get返回新值
+      get() {
+        if (this.cartList) {
+          return this.cartList.every((shop) => {
+            return shop.checked === true
+          })
+        }
+        return false
+      },
+      set(value) {
+        this.cartList.forEach((shop)=>{
+          shop.checked=value
+          shop.goodsList.forEach((good)=>{
+            good.checked=value
+          })
+        })
+      }
+    }
   },
   created() {
     this.getCartList()
@@ -21,18 +43,34 @@ new Vue({
     getCartList() {
       axios.get(url.cartList).then(res => {
         //先加属性再给data赋值实现响应式
-        res.data.cartList.forEach((shop)=>{
-          shop.checked=true
-          shop.goodsList.forEach((good)=>{
-            good.checked=true
+        res.data.cartList.forEach((shop) => {
+          shop.checked = true
+          shop.goodsList.forEach((good) => {
+            good.checked = true
           })
         })
 
-        this.cartList= res.data.cartList
+        this.cartList = res.data.cartList
       })
     },
-    selectGood(good){
-      good.checked=!good.checked
+    selectGood(good, shop) {
+      good.checked = !good.checked
+
+      shop.checked = shop.goodsList.every((good) => {   //every方法检验每个元素是否符合要求，根据全部判断结果返回值
+        return good.checked === true
+      })
+
+
+    },
+    selectShop(shop) {
+      shop.checked = !shop.checked
+      shop.goodsList.forEach((good) => {
+        good.checked = shop.checked
+      })
+    },
+    selectAll() {
+      this.isAll = !this.isAll
+      console.log(this.isAll);
     }
   },
   mixins: [Mixin]
